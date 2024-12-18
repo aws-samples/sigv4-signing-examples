285e86c61fb839ce01fc592b662d65d9e2df6793
Josh Hart <josharj@amazon.co.uk>
2024-12-18 17:10:14 +0000

    Migrate nodejs sample to awssdk

--- sdk/nodejs/main.js
+++ sdk/nodejs/main.js
@@ -1,48 +1,61 @@
 const https = require('https');
-const aws4 = require('aws4');
+const { SignatureV4 } = require('@aws-sdk/signature-v4');
+const { Sha256 } = require('@aws-crypto/sha256-js');
 
-const accessKey = process.env.AWS_ACCESS_KEY_ID;
-const secretKey = process.env.AWS_SECRET_ACCESS_KEY;
-const sessionToken = process.env.AWS_SESSION_TOKEN;
-const service = 'execute-api';
-const host = process.env.RESTAPIHOST;
-const canonicalURI = process.env.RESTAPIPATH;
-const region = 'us-east-1';
+async function makeSignedRequest() {
+  const accessKey = process.env.AWS_ACCESS_KEY_ID;
+  const secretKey = process.env.AWS_SECRET_ACCESS_KEY;
+  const sessionToken = process.env.AWS_SESSION_TOKEN;
+  const service = 'execute-api';
+  const host = process.env.RESTAPIHOST;
+  const canonicalURI = process.env.RESTAPIPATH;
+  const region = 'us-east-1';
 
-const options = {
-  hostname: host,
-  path: canonicalURI,
-  method: 'GET',
-  headers: {
-    'Host': host,
-  },
-};
+  const options = {
+    hostname: host,
+    path: canonicalURI,
+    method: 'GET',
+    headers: {
+      'Host': host,
+    },
+  };
 
-const signer = aws4.sign({
-  service: service,
-  region: region,
-  path: canonicalURI,
-  headers: options.headers,
-  method: options.method,
-  body: '',
-}, {
-  accessKeyId: accessKey,
-  secretAccessKey: secretKey,
-  sessionToken: sessionToken,
-});
-
-Object.assign(options.headers, signer.headers);
-
-const req = https.request(options, (res) => {
-  console.log(`response Status: ${res.statusCode}`);
-
-  res.on('data', (chunk) => {
-    console.log(`response Body: ${chunk}`);
+  const signer = new SignatureV4({
+    credentials: {
+      accessKeyId: accessKey,
+      secretAccessKey: secretKey,
+      sessionToken: sessionToken
+    },
+    region: region,
+    service: service,
+    sha256: Sha256
   });
-});
 
-req.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
-});
+  const signedRequest = await signer.sign({
+    method: options.method,
+    headers: options.headers,
+    hostname: host,
+    path: canonicalURI,
+    protocol: 'https:'
+  });
 
-req.end();
+  Object.assign(options.headers, signedRequest.headers);
+
+  const req = https.request(options, (res) => {
+    console.log(`response Status: ${res.statusCode}`);
+
+    res.on('data', (chunk) => {
+      console.log(`response Body: ${chunk}`);
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  req.end();
+}
+
+makeSignedRequest().catch(error => {
+  console.error('Error:', error);
+});
