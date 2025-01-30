e3bce100afb00b2a8a9f5c57e131a5ae8f1a17c7
William Desportes <williamdes@wdes.fr>
2025-01-30 11:00:30 +0100

    Fix the no-sdk nodejs implementation

--- no-sdk/nodejs/main.js
+++ no-sdk/nodejs/main.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const https = require('https');
 
-// AWS access keys 
+// AWS access keys
 const accessKey = process.env.AWS_ACCESS_KEY_ID;
 const secretKey = process.env.AWS_SECRET_ACCESS_KEY;
 const sessionToken = process.env.AWS_SESSION_TOKEN;
@@ -10,7 +10,7 @@ const sessionToken = process.env.AWS_SESSION_TOKEN;
 const method = 'GET';
 const service = 'execute-api';
 const host = process.env.RESTAPIHOST;
-const region = 'us-east-1';
+const region = process.env.AWS_DEFAULT_REGION || 'us-east-1';
 const endpoint = process.env.RESTAPIPATH;
 
 // Create a datetime object for signing
@@ -27,16 +27,70 @@ const dateStamp = amzDate.slice(0, 8);
 // Create the canonical request
 const canonicalUri = endpoint;
 const canonicalQuerystring = '';
-const canonicalHeaders = `host:${host}\n`;
-const signedHeaders = 'host';
-const payloadHash = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'; // SHA-256 hash of an empty string
-const canonicalRequest = `${method}\n${canonicalUri}\n${canonicalQuerystring}\n${canonicalHeaders}\n${signedHeaders}\n${payloadHash}`;
+const requestBody = '';
+const requestBodyHash = crypto.createHash('sha256').update(requestBody).digest('hex');
+const headers = {
+  'Host': host,
+  'X-Amz-Date': amzDate,
+  'X-Amz-Content-Sha256': requestBodyHash,
+};
+if (sessionToken) {
+  headers['X-Amz-Security-Token'] = sessionToken;
+}
+
+const canonicalRequest = createCanonicalRequest(method, endpoint, canonicalQuerystring, headers, requestBodyHash);
 
 // Create the string to sign
 const algorithm = 'AWS4-HMAC-SHA256';
 const credentialScope = `${dateStamp}/${region}/${service}/aws4_request`;
-const hashCanonicalRequest = crypto.createHash('sha256').update(canonicalRequest).digest('hex');
-const stringToSign = `${algorithm}\n${amzDate}\n${credentialScope}\n${hashCanonicalRequest}`;
+
+function getSignedHeaders(headers) {
+  return Object.keys(headers)
+    .map(key => key.toLowerCase())
+    .filter(key => true)
+    .sort()
+    .join(';');
+}
+
+function getCanonicalPath(path, isS3) {
+    if (!isS3) {
+        // Non-S3 services, we normalize the path and then double URI encode it.
+        // Ref: "Remove Dot Segments" https://datatracker.ietf.org/doc/html/rfc3986#section-5.2.4
+        const normalizedPathSegments = [];
+        for (const pathSegment of path.split("/")) {
+            if (pathSegment?.length === 0)
+                continue;
+            if (pathSegment === ".")
+                continue;
+            if (pathSegment === "..") {
+                normalizedPathSegments.pop();
+            }
+            else {
+                normalizedPathSegments.push(pathSegment);
+            }
+        }
+        // Joining by single slashes to remove consecutive slashes.
+        const normalizedPath = `${path?.startsWith("/") ? "/" : ""}${normalizedPathSegments.join("/")}${normalizedPathSegments.length > 0 && path?.endsWith("/") ? "/" : ""}`;
+        const doubleEncoded = encodeURIComponent(normalizedPath);
+        return doubleEncoded.replace(/%2F/g, "/");
+    }
+    // For S3, we shouldn't normalize the path. For example, object name
+    // my-object//example//photo.user should not be normalized to
+    // my-object/example/photo.user
+    return path;
+}
+
+// Function to create a canonical request for AWS Signature Version 4
+function createCanonicalRequest(method, path, queryParams, headers, payloadHash) {
+    path = getCanonicalPath(path, service === 's3');
+    const canonicalHeaders = Object.keys(headers)
+        .map(key => `${key.toLowerCase()}:${headers[key].trim()}\n`)
+        .sort()
+        .join('');
+
+    const signedHeaders = getSignedHeaders(headers);
+    return `${method}\n${path}\n${queryParams}\n${canonicalHeaders}\n${signedHeaders}\n${payloadHash}`;
+}
 
 // Sign the string
 const getSignatureKey = (key, dateStamp, regionName, serviceName) => {
@@ -47,10 +101,17 @@ const getSignatureKey = (key, dateStamp, regionName, serviceName) => {
   return kSigning;
 };
 const signingKey = getSignatureKey(secretKey, dateStamp, region, service);
+
+function getSignatureSubject(algorithm, amzDate, scope, canonicalRequest) {
+  let hashedCanonicalRequest = crypto.createHash("sha256").update(canonicalRequest).digest("hex");
+  return `${algorithm}\n${amzDate}\n${scope}\n${hashedCanonicalRequest}`;
+}
+
+const stringToSign = getSignatureSubject(algorithm, amzDate, credentialScope, canonicalRequest);
 const signature = crypto.createHmac('sha256', signingKey).update(stringToSign).digest('hex');
 
 // Add signing information to the request
-const authorizationHeader = `${algorithm} Credential=${accessKey}/${credentialScope}, SignedHeaders=${signedHeaders}, Signature=${signature}`;
+const authorizationHeader = `${algorithm} Credential=${accessKey}/${credentialScope}, SignedHeaders=${getSignedHeaders(headers)}, Signature=${signature}`;
 
 // Make the request
 const options = {
@@ -58,9 +119,7 @@ const options = {
   path: canonicalUri,
   method,
   headers: {
-    'host': host,
-    'x-amz-date': amzDate,
-    'x-amz-security-token': sessionToken,
+    ...headers,
     'Authorization': authorizationHeader
   }
 };
