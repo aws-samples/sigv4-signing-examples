effe422dad1f497c876da9ac1cec263608fb80b1
Josh Hart <josharj@amazon.co.uk>
2025-02-17 11:00:41 +0000

    refactor java entry point: add region logic and move to getenv

--- no-sdk/java/Program.java
+++ no-sdk/java/Program.java
@@ -6,20 +6,22 @@ import sigv4.signing.samples.BedrockConverseSample;
 public class Program {
     public static void main(String[] args) {
 
+        String region = System.getenv("AWS_DEFAULT_REGION") != null 
+            ? System.getenv("AWS_DEFAULT_REGION") 
+            : "us-east-1";
+
         if (args.length == 0 || args[0].equals("ApiGatewaySample")) {
             System.out.println("\n\n*****************API Gateway Sample*******************************");
-            String RESTAPIHOST = System.getProperty("RESTAPIHOST");
-            String RESTAPIPATH = System.getProperty("RESTAPIPATH");
-            String apiRegion = "eu-west-2";
-            ApiGatewaySample.run(apiRegion, RESTAPIHOST, RESTAPIPATH);
+            String RESTAPIHOST = System.getenv("RESTAPIHOST");
+            String RESTAPIPATH = System.getenv("RESTAPIPATH");
+            ApiGatewaySample.run(region, RESTAPIHOST, RESTAPIPATH);
         }
 
         if (args.length == 0 || args[0].equals("BedrockConverseSample")) {
             System.out.println("\n\n*****************Bedrock Converse Sample*******************************");
             String modelId = "amazon.titan-text-express-v1";
-            String bedrockRegion = "us-east-1";
             String prompt = "What is the capital of England?";
-            BedrockConverseSample.run(bedrockRegion, modelId, prompt);
+            BedrockConverseSample.run(region, modelId, prompt);
         }
     }
 }
